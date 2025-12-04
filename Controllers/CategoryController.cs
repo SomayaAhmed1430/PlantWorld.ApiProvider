93d4c9f2d1be5347b82fd665a03670adf9242d33
SomayaAhmed1430 <s.ramadan02185@dmu.edu.eg>
2025-12-04 23:06:18 +0200

    0

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PlantWorld.ApiProvider.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+    }
+}
